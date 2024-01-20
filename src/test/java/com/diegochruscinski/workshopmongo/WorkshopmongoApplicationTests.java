0e71580e8c1d33ce54795d83abe60b4573676c6d
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-20 15:56:10 -0300

    First Commit

---
+++ src/test/java/com/diegochruscinski/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegochruscinski.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
