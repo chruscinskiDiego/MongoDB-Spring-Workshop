3d30057d8fe3793135aa8e88cf83a85635b1064b
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-20 17:29:18 -0300

    Connection in MongoDB

--- src/main/java/com/diegochruscinski/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diegochruscinski/workshopmongo/resources/UserResource.java
@@ -4,23 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diegochruscinski.workshopmongo.domain.User;
+import com.diegochruscinski.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User diego = new User("1","Diego Chruscinski de Souza", "contato@diegochruscinski.com.br");
-		User milena = new User("2","Milena Chruscinski de Souza", "contato@milenachruscinski.com.br");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(diego,milena));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
