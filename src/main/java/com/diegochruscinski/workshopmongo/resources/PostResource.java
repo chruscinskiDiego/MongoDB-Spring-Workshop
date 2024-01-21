e1f4139a36202e9c1def51c2f43bd13e0a17fc57
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-21 12:00:59 -0300

    Posts Query

--- src/main/java/com/diegochruscinski/workshopmongo/resources/PostResource.java
+++ src/main/java/com/diegochruscinski/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.diegochruscinski.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diegochruscinski.workshopmongo.domain.Post;
+import com.diegochruscinski.workshopmongo.resources.util.URL;
 import com.diegochruscinski.workshopmongo.services.PostService;
 
 @RestController
@@ -24,6 +28,16 @@ public class PostResource {
 		Post obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/titlesearch")
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value ="text", defaultValue = "") String text){
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
 	}
 	
 	
