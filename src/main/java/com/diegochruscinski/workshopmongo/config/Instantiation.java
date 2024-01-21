9acd7c81abfe7d8e130f709327ab7f2a75cb414a
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-01-21 11:38:23 -0300

    Comments Added

--- src/main/java/com/diegochruscinski/workshopmongo/config/Instantiation.java
+++ src/main/java/com/diegochruscinski/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.diegochruscinski.workshopmongo.domain.Post;
 import com.diegochruscinski.workshopmongo.domain.User;
 import com.diegochruscinski.workshopmongo.dto.AuthorDTO;
+import com.diegochruscinski.workshopmongo.dto.CommentDTO;
 import com.diegochruscinski.workshopmongo.repositories.PostRepository;
 import com.diegochruscinski.workshopmongo.repositories.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Lets get it","Go travel!!", new AuthorDTO(alex));
 		Post post2 = new Post(null, sdf.parse("01/01/2024"),"Party!!!","Happy new Year!!", new AuthorDTO(alex));
+		
+		CommentDTO c1 = new CommentDTO("Good Party!", sdf.parse("10/10/2023"), new AuthorDTO(maria));
+		CommentDTO c2 = new CommentDTO("Nice trip!", sdf.parse("10/10/2023"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Congratulations!", sdf.parse("10/10/2023"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
